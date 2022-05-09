887e7223936d71e6cbfa6f5a04cc7fd0cd9882e9
Magdalena Checinski <madzix.p730@gmail.com>
2022-05-09 14:05:37 +0200

    Added global styles

---
+++ src/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyle = createGlobalStyle`
+html{
+box-sizing:border-box;
+}
+body{
+    font-family: "Montsserat", sans-serif;
+    margin:0;
+    padding:0;
+}
+`;
