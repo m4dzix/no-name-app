58c3820d9a84019f788b7f7f4fbb69c4b52f5bda
Magdalena Checinski <madzix.p730@gmail.com>
2022-05-19 23:35:01 +0200

    Created background

--- src/common/Background/index.js
+++ src/common/Background/index.js
@@ -1,5 +1,14 @@
 import { Container } from "./style.js";
+import Text from "../../Assets/Tukan_bay.png";
+import ChooseField from "../ChooseField";
 
-const Background = () => <Container />;
+const Background = () => (
+  <>
+    <Container>
+      <img src={Text} alt="logo" />
+    </Container>
+    <ChooseField />
+  </>
+);
 
 export default Background;
