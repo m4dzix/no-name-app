fd206ffc0cf2c69330329c8c59b73da2f5f30ab7
Magdalena Checinski <madzix.p730@gmail.com>
2022-05-19 23:40:33 +0200

    Changed navigation width and color

--- src/common/Navigation/style.js
+++ src/common/Navigation/style.js
@@ -9,7 +9,7 @@ export const Nav = styled.nav`
   top: 0;
   left: 0;
   position: fixed;
-  width: 100vw;
+  width: calc(100vw - 60px);
 `;
 export const Logo = styled.img`
   max-width: 192px;
@@ -17,7 +17,8 @@ export const Logo = styled.img`
 `;
 export const List = styled.ul`
   list-style: none;
-  font-family: "PT Sans Narrow", sans-serif;
+  font-family: "Roboto", sans-serif;
+  font-weight: 700;
   text-transform: uppercase;
   margin: 0;
   padding: 0;
@@ -28,11 +29,13 @@ export const List = styled.ul`
 `;
 export const Item = styled.li`
   font-size: 13px;
-  padding-right: 10px;
+  padding-right: 15px;
+  color: white;
+  text-shadow: 0.5px 0.5px black;
 `;
 export const Button = styled.li`
-  border: 0.5px solid black;
-  padding: 5px 35px;
+  border: 0.5px solid white;
+  padding: 15px 30px;
   background-color: transparent;
 `;
 export const MenuIcon = styled(Menu)`
