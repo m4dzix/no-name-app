8257f49eb27142e84bdcc239c165ae6c08208a80
Magdalena Checinski <madzix.p730@gmail.com>
2022-06-24 16:48:29 +0200

    Created popups

--- src/common/ChooseField/index.js
+++ src/common/ChooseField/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   CheckInBox,
   Text,
@@ -5,49 +6,124 @@ import {
   Item,
   Box,
   StyledSpan,
-  //   StyledInput,
   Arrow,
   StyledButton,
+  Popup,
+  Number,
+  CounterButton,
+  StyledPlus,
+  StyledMinus,
 } from "./style.js";
 
-const ChooseField = () => (
-  <CheckInBox>
-    <Text>Book your stay</Text>
-    <List>
-      <Item>
-        I want
-        <Box>
-          <StyledSpan>1</StyledSpan>Room <Arrow />
-        </Box>
-      </Item>
-      <Item>
-        from
-        <Box>
-          <StyledSpan>May 19</StyledSpan>
-          <StyledSpan>-</StyledSpan>
-          <StyledSpan>May 20</StyledSpan>
-          <Arrow />
-        </Box>
-      </Item>
-      <Item>
-        for
-        <Box>
-          <StyledSpan>1</StyledSpan>
-          Adult
-          <Arrow />
-        </Box>
-        <StyledSpan>•</StyledSpan>
-        <Box>
-          <StyledSpan>0</StyledSpan>
-          Children
-          <Arrow />
-        </Box>
-      </Item>
-      <Item>
-        <StyledButton>Book a room</StyledButton>
-      </Item>
-    </List>
-  </CheckInBox>
-);
+const ChooseField = () => {
+  const [room, setRoom] = useState(1);
+  const [adult, setAdult] = useState(1);
+  const [child, setChild] = useState(0);
+
+  const [activeRoom, setActiveRoom] = useState(false);
+  const [activeCalendar, setActiveCalendar] = useState(false);
+  const [activeAdult, setActiveAdult] = useState(false);
+  const [activeChild, setActiveChild] = useState(false);
+
+  const increase = (setCounter, counter) => setCounter(counter + 1);
+  const decrease = (setCounter, counter) => setCounter(counter - 1);
+  const toggleActive = (active, setActive) => () => setActive(!active);
+  return (
+    <CheckInBox>
+      <Text>Book your stay</Text>
+      <List>
+        <Item>
+          I want
+          <Box>
+            <StyledSpan>{room}</StyledSpan> {room <= 1 ? "Room" : "Rooms"}{" "}
+            <Arrow
+              role="button"
+              onClick={toggleActive(activeRoom, setActiveRoom)}
+            />
+            <Popup activeClass={activeRoom}>
+              <Number>{room}</Number>
+              {room <= 1 ? "Room" : "Rooms"}{" "}
+              <CounterButton
+                disabled={room === 20}
+                onClick={() => increase(setRoom, room)}
+              >
+                <StyledPlus />
+              </CounterButton>
+              <CounterButton
+                disabled={room === 0}
+                onClick={() => decrease(setRoom, room)}
+              >
+                <StyledMinus />
+              </CounterButton>
+            </Popup>
+          </Box>
+        </Item>
+        <Item>
+          from
+          <Box>
+            <StyledSpan>May 19</StyledSpan>
+            <StyledSpan>-</StyledSpan>
+            <StyledSpan>May 20</StyledSpan>
+            <Arrow onClick={toggleActive(activeCalendar, setActiveCalendar)} />
+            <Popup activeClass={activeCalendar}></Popup>
+          </Box>
+        </Item>
+        <Item>
+          for
+          <Box>
+            <StyledSpan>{adult}</StyledSpan>
+            {adult <= 1 ? "Adult" : "Adults"}
+            <Arrow
+              role="button"
+              onClick={toggleActive(activeAdult, setActiveAdult)}
+            />
+            <Popup activeClass={activeAdult}>
+              <Number>{adult}</Number> {adult <= 1 ? "Adult" : "Adults"}{" "}
+              <CounterButton
+                disabled={adult === 20}
+                onClick={() => increase(setAdult, adult)}
+              >
+                <StyledPlus />
+              </CounterButton>
+              <CounterButton
+                disabled={adult === 0}
+                onClick={() => decrease(setAdult, adult)}
+              >
+                {" "}
+                <StyledMinus />
+              </CounterButton>
+            </Popup>
+          </Box>
+          <StyledSpan>•</StyledSpan>
+          <Box>
+            <StyledSpan>{child}</StyledSpan>
+            {child <= 1 ? "Children" : "Childrens"}
+            <Arrow onClick={toggleActive(activeChild, setActiveChild)} />
+            <Popup activeClass={activeChild}>
+              {" "}
+              <Number>{child}</Number> {child <= 1 ? "Children" : "Childrens"}{" "}
+              <CounterButton
+                disabled={child === 20}
+                onClick={() => increase(setChild, child)}
+              >
+                <StyledPlus />
+              </CounterButton>
+              <CounterButton
+                disabled={child === 0}
+                onClick={() => decrease(setChild, child)}
+              >
+                {" "}
+                <StyledMinus />
+              </CounterButton>
+            </Popup>
+          </Box>
+        </Item>
+        <Item>
+          <StyledButton>Book a room</StyledButton>
+        </Item>
+      </List>
+    </CheckInBox>
+  );
+};
 
 export default ChooseField;
--- src/common/ChooseField/style.js
+++ src/common/ChooseField/style.js
@@ -1,5 +1,6 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { KeyboardArrowDown } from "@styled-icons/material-outlined";
+import { Plus, Minus } from "@styled-icons/feather";
 
 export const CheckInBox = styled.div`
   position: absolute;
@@ -42,6 +43,7 @@ export const Box = styled.div`
   margin: 0 3px;
   padding-bottom: 3px;
   border-bottom: 2px dotted white;
+  position: relative;
 `;
 export const StyledSpan = styled.span`
   font-size: 18px;
@@ -52,19 +54,80 @@ export const Arrow = styled(KeyboardArrowDown)`
   height: 20px;
   color: white;
   margin: 0 3px;
+  cursor: pointer;
 `;
 export const StyledButton = styled.button`
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  padding: 0 30px;
+  padding: 10px 30px;
   font-size: 20px;
   font-weight: 700;
   color: white;
   background-color: #00a4aa;
   border: 1px solid #00a4aa;
-  height: 45px;
   font-size: 18px;
   text-shadow: 0.5px 0.5px black;
   text-transform: uppercase;
+`;
+
+export const Popup = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 100%;
+  right: 0%;
+  z-index: 5;
+  padding: 15px 10px;
+  border-radius: 5px;
+  margin-left: -105px;
+  margin-bottom: 12px;
+  background: #fff;
+  color: #242424;
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 0.3s, visibility 0.3s;
+  box-shadow: 0 5px 8px rgb(0 0 0 / 16%);
+  ${({ activeClass }) =>
+    activeClass &&
+    css`
+      opacity: 1;
+      visibility: visible;
+    `}
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 13px;
+    left: 50%;
+    width: 0;
+    height: 0;
+    border: 20px solid transparent;
+    border-top-color: white;
+    border-bottom: 0;
+    margin-left: -20px;
+    margin-bottom: -20px;
+  }
+`;
+export const Number = styled.p`
+  margin-right: 8px;
+`;
+export const CounterButton = styled.button`
+  margin-left: 8px;
+  background-color: #00a4aa;
+  color: white;
+  border: 1px solid white;
+  padding: 5px;
+`;
+
+export const StyledCounter = css`
+  width: 20px;
+  height: 20px;
+  color: white;
+`;
+export const StyledPlus = styled(Plus)`
+  ${StyledCounter}
+`;
+export const StyledMinus = styled(Minus)`
+  ${StyledCounter}
 `;
