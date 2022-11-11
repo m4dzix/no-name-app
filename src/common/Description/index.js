a76ec9fdf7fe217760bf96d82d5030d9381bc1fb
Magdalena Checinski <madzix.p730@gmail.com>
2022-11-11 18:33:20 +0100

    "aos" used for animation

--- src/common/Description/index.js
+++ src/common/Description/index.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import { Text } from "./styled.js";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Description = ({ text }) => {
-  return <Text>{text}</Text>;
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return <Text data-aos="fade">${text}</Text>;
 };
 
 export default Description;
--- src/common/Image/index.js
+++ src/common/Image/index.js
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+
 import { StyledImage } from "./style.js";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Image = ({ image, big }) => {
-  return <StyledImage big={big} image={image} />;
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return <StyledImage data-aos="fade-up" big={big} image={image} />;
 };
 export default Image;
